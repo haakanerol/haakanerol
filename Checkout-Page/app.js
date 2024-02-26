d324300f11750af36d3a0f1b1272797d526ce441
hakanerol <hakan-erol@outlook.com>
2024-02-26 20:05:30 +0100

    plus button activated

--- Checkout-Page/app.js
+++ Checkout-Page/app.js
@@ -1,1 +1,36 @@
-//Kodları buraya yazınız.
+// selectors
+const deleteAll = document.querySelector(".delete-div .fa-trash-can")
+const products = document.querySelector(".products")
+
+
+
+
+// event handlers
+
+deleteAll.addEventListener("click",()=>{
+   products.textContent = "No Products"
+   products.classList.add("no-product")
+   document.querySelector(".delete-div").style.display = "none"
+})
+
+products.addEventListener("click",(e)=>{
+    console.log(e.target)
+    if(e.target.classList.contains("fa-plus")){
+        e.target.previousElementSibling.textContent++
+        calcPrice(e.target)
+
+    }else if (e.target.classList.contains("fa-minus")){
+
+    }else if (e.target.classList.contains("fa-trash-can")){
+       
+    }
+
+
+})
+
+const calcPrice = (btn)=>{
+    const discountPrice = btn.closest(".product-info")
+                             .querySelector("#discounted-price")
+         console.log(discountPrice.textContent);                    
+
+}
