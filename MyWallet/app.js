fcec6d5142c3757ca5c7017ffa4b9a8a039f2fcc
hakanerol <hakan-erol@outlook.com>
2024-03-05 17:53:06 +0100

    Js done partly

--- MyWallet/app.js
+++ MyWallet/app.js
@@ -27,6 +27,7 @@ btnAdd.addEventListener('click', ()=>{
     totalIncome += Number(inputIncome.value);
     inputIncome.value = '';
     localStorage.setItem('totalIncome', totalIncome);
+    incomeTd.textContent = totalIncome;
 });
 
 
@@ -41,10 +42,94 @@ expensesForm.addEventListener('submit', (e)=>{
     }
     expensesList.push(newExpense);
     localStorage.setItem('expensesList', JSON.stringify(expensesList));
+    setExpensesDom(newExpense)
     expensesForm.reset();
-    dateInput.value = '';
-    console.log(expensesList);
+    dateInput.valueAsDate = new Date();
+    calcTotal();
   
 });
+
+expensesBody.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-trash-can')) {
+        e.target.parentNode.parentNode.remove();
+        expensesList = expensesList.filter(expense => expense.id != e.target.id);
+        localStorage.setItem('expensesList', JSON.stringify(expensesList));
+        calcTotal();
+    }
+})
+
+btnReset.addEventListener('click', ()=>{
+    if(confirm('Are you sure you want to reset?')){
+        expensesList = [];
+        localStorage.removeItem('expensesList');
+        localStorage.removeItem('totalIncome');
+        localStorage.removeItem('totalExpenses');
+        localStorage.removeItem('totalRest');
+        totalIncome = 0
+        totalExpenses = 0
+        totalRest = 0
+        expensesBody.innerHTML = '';
+        calcTotal();
+    }
+
+});
+
+window.addEventListener('load',()=>{
+    totalIncome = Number(localStorage.getItem('totalIncome')) || 0;
+    expensesList = JSON.parse(localStorage.getItem('expensesList')) || [];
+    expensesList.foreach(expens => setExpensesDom(expens));
+    incomeTd.textContent = totalIncome;
+    calcTotal();
+})
+
+// Functions
+
+const setExpensesDom = ({date, type, amount, id})=>{
+
+    const tr = document.createElement('tr');
+
+    const appendTd = (content)=>{
+        const td = document.createElement('td');
+        td.textContent = content;
+        return td;
+    }
+
+    const addTrashTd  = () => {
+
+        const td = document.createElement('td');
+        const trash = document.createElement('i');
+        trash.classList.add('fa', 'fa-trash-can', 'text-warning');
+        trash.type = 'button';
+        trash.id = id;
+        td.appendChild(trash);
+        return td;
+
+    };
+
+    tr.append(
+        appendTd(date),
+        appendTd(type),
+        appendTd(amount),
+        addTrashTd()
+    )
+    expensesBody.append(tr);
+    
+    };
+
+    const calcTotal = () => {
+        const spendings = expensesList.reduce((acc, curr) => 
+             acc + Number(curr.amount), 0);
+
+        incomeTd.textContent = totalIncome;
+        expensesTd.textContent = spendings;
+        restTd.textContent = totalIncome - spendings;
+
+    };
+
+ 
+
+  
+   
+    
 
 
